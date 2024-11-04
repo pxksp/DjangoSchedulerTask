00f6f606db0d03be9842defce222c8c4e62c7236
PangPang <xxkkss@xxkkss.com>
2024-11-04 15:09:49 +0800

    添加基础添加任务接口

--- apps/tasks/urls.py
+++ apps/tasks/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from apps.tasks.views import *
 
 urlpatterns = [
-    path('', AddTaskApi.as_view(), name='add_task'),
+    path('tasks/add/', AddTaskApi.as_view(), name='add_task'),
 ]
--- apps/tasks/views.py
+++ apps/tasks/views.py
@@ -1,3 +1,74 @@
-from django.shortcuts import render
+import random
+import time
 
-# Create your views here.
+from apscheduler.triggers.cron import CronTrigger
+from django.http import JsonResponse
+from django.views.generic import View
+
+from apps.tasks.jobs import scheduler
+from apps.tasks.models import JobInfo
+
+
+def run_scrapy(*args, **kwargs):
+    job_name = kwargs.get('job_name')
+    job_info = JobInfo.objects.get(job_name=job_name)
+    if job_info.job_status == 'stop':
+        return False
+    job_id = job_info.job_id
+    job = scheduler.get_job(job_id)
+    job_info.job_next_run_time = job.next_run_time
+    job_info.job_start_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+    job_info.job_status = 'running'
+    job_info.save()
+    try:
+        time.sleep(random.randint(1, 5))
+        print('scrapy run success')
+        job_info.job_success_count += 1
+        job_info.job_end_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+        job_info.job_status = 'success'
+        job_info.save()
+        return True
+    except Exception as e:
+        job_info.job_fail_count += 1
+        job_info.job_end_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+        job_info.job_status = 'fail'
+        job_info.save()
+        return False
+
+
+class AddTaskApi(View):
+    """
+    添加任务接口
+    """
+
+    @classmethod
+    def post(cls, request, *args, **kwargs):
+        job_name = request.POST.get('job_name')
+        job_cron = request.POST.get('job_cron')
+        job_func = request.POST.get('job_func')
+        job_args = request.POST.get('job_args')
+        job_kwargs = request.POST.get('job_kwargs')
+        if not all([job_name, job_cron, job_func, job_args, job_kwargs]):
+            return JsonResponse({'code': 400, 'msg': '参数错误'})
+        if JobInfo.objects.filter(job_name=job_name).exists():
+            return JsonResponse({'code': 400, 'msg': '任务已存在'})
+        job_args = eval(job_args) if job_args else []
+        job_kwargs = eval(job_kwargs) if job_kwargs else {}
+        job_func = eval(job_func)
+        job_status = 'init'
+        job_kwargs['job_name'] = job_name
+        job_cron_list = job_cron.split(' ')
+        trigger = CronTrigger(second=job_cron_list[0], minute=job_cron_list[1], hour=job_cron_list[2], day=job_cron_list[3], month=job_cron_list[4], day_of_week=job_cron_list[5])
+        job = scheduler.add_job(job_func, trigger, args=job_args, kwargs=job_kwargs, max_instances=10)
+        job_id = job.id
+        JobInfo.objects.create(
+            job_id=job_id,
+            job_name=job_name,
+            job_status=job_status,
+            job_cron=job_cron,
+            job_func=job_func,
+            job_args=job_args,
+            job_kwargs=job_kwargs,
+            job_next_run_time=job.next_run_time
+        )
+        return JsonResponse({'code': 200, 'msg': '添加成功'})
