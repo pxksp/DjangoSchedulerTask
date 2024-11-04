91abc7298066e56d8e36f20c2a434886d574a963
PangPang <xxkkss@xxkkss.com>
2024-11-04 15:08:22 +0800

    添加任务信息数据模型

--- apps/tasks/models.py
+++ apps/tasks/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 
 class JobInfo(models.Model):
@@ -9,14 +10,13 @@ class JobInfo(models.Model):
     job_func = models.CharField(max_length=64, verbose_name='任务函数')
     job_args = models.CharField(max_length=64, verbose_name='任务参数')
     job_kwargs = models.CharField(max_length=64, verbose_name='任务kwargs')
-    job_instance = models.CharField(max_length=64, verbose_name='任务实例')
-    job_trigger = models.CharField(max_length=64, verbose_name='任务触发器')
+    job_trigger = models.CharField(max_length=64, default="CronTrigge", verbose_name='任务触发器', )
     job_next_run_time = models.CharField(max_length=64, verbose_name='下次运行时间')
-    job_success_count = models.IntegerField(verbose_name='成功次数')
-    job_fail_count = models.IntegerField(verbose_name='失败次数')
+    job_success_count = models.IntegerField(verbose_name='成功次数', default=0)
+    job_fail_count = models.IntegerField(verbose_name='失败次数', default=0)
     job_start_time = models.CharField(max_length=64, verbose_name='开始时间')
     job_end_time = models.CharField(max_length=64, verbose_name='结束时间')
-    job_create_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
+    job_create_time = models.DateTimeField(default=timezone.now, verbose_name='创建时间')
     job_update_time = models.DateTimeField(auto_now=True, verbose_name='更新时间')
 
     class Meta:
